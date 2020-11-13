38807681e553f035880a383cccefb362ea7a4d9a
Alfred <alyip98@gmail.com>
2020-11-13 16:29:59 +0800

    Move index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!') + JSON.stringify(event),
+        body: JSON.stringify(event),
     };
     return response;
 };
